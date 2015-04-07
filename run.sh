99ec3aced77f2e22ba0576b01d47bbc5e84817ec
Steve R <gitstever@gmail.com>
2015-04-07 12:02:39 -0400

    added nginx test

--- run.sh
+++ run.sh
@@ -15,7 +15,11 @@ do
 done
 
 sudo openssl pkcs12 -in ${dir}/localhost.localdomain.p12 -nocerts -out /etc/pki/tls/private/localhost.key -nodes -passin pass:password
+sudo chown root:root /etc/pki/tls/private/localhost.key
+sudo chmod 640 /etc/pki/tls/private/localhost.key
 sudo openssl pkcs12 -in ${dir}/localhost.localdomain.p12 -nokeys -out /etc/pki/tls/certs/localhost.crt -passin pass:password
+sudo chown root:root /etc/pki/tls/certs/localhost.crt
+sudo chmod 644 /etc/pki/tls/certs/localhost.crt
 echo 'SSLCACertificateFile /etc/pki/tls/certs/ca-bundle.crt
 SSLVerifyClient require
 SSLVerifyDepth 10
@@ -39,22 +43,26 @@ sudo rm -rf /etc/pki/tls/crls
 if [ ! -d /etc/pki/tls/crls ]
 then
 	sudo mkdir /etc/pki/tls/crls
-	chown ${USER}:root /etc/pki/tls/crls
+	sudo chown ${USER}:root /etc/pki/tls/crls
 	chmod 755 /etc/pki/tls/crls
 fi
 for name in $(ls -1 ${dir} | grep ^.*\.crl\.pem$)
 do
 	cp -p ${dir}/${name} /etc/pki/tls/crls/${name}
-	#chown root:root /etc/pki/tls/crls/${name}
 	chmod 644 /etc/pki/tls/crls/${name}
 done
-c_rehash /etc/pki/tls/crls/ >/dev/null
+sudo c_rehash /etc/pki/tls/crls/ >/dev/null
+sudo chown -R root:root /etc/pki/tls/crls
 if [ ! -f /var/www/html/index.html ]
 then
 	echo '<html><head></head><body>Hello</body></html>' > /var/www/html/index.html
 	chmod 644 /var/www/html/index.html
 fi
+sudo service nginx stop
 sudo service httpd restart
+
+test_https ()
+{
 for name in a b c d
 do
 	for version in sslv2 sslv3 tlsv1
@@ -81,7 +89,32 @@ do
 		fi
 	done
 done
-echo
+}
+
+test_https
+
+sudo service httpd stop
+
+echo configuring nginx
+cat $(dirname $0)/nginx/ssl.conf | sudo tee /etc/nginx/conf.d/ssl.conf
+cat $(dirname $0)/nginx/nginx.conf | sudo tee /etc/nginx/nginx.conf
+sudo chown root:root /etc/nginx/conf.d/ssl.conf /etc/nginx/nginx.conf
+sudo chmod 644 /etc/nginx/conf.d/ssl.conf /etc/nginx/nginx.conf
+cat ${dir}/Test_CA.crl.pem ${dir}/Test_Intermediate_CA.crl.pem | sudo tee /etc/nginx/crl.pem
+sudo chown root:root /etc/nginx/crl.pem
+sudo chmod 644 /etc/nginx/crl.pem
+cat ${dir}/Test_CA.pem ${dir}/Test_Intermediate_CA.pem | sudo tee /etc/nginx/ca.pem
+sudo chown root:root /etc/nginx/ca.pem
+sudo chmod 644 /etc/nginx/ca.pem
+sudo cp -p /etc/pki/tls/certs/localhost.crt /etc/nginx/cert.pem
+sudo cp -p /etc/pki/tls/private/localhost.key /etc/nginx/cert.key
+sudo service nginx start
+
+test_https
+
+sudo service nginx stop
+sudo service httpd start
+
 ls -1 ${dir} | grep ^.\.pem$ | xargs -I {} openssl verify -crl_check -CApath ${dir} ${dir}/{}
 echo
 echo certutil -V -d ${nssdir} -n d -u C
